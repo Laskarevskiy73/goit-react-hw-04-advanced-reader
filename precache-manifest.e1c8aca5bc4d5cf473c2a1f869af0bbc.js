a478751a020882e30cd87eb1b10163d1f9157bb3
Laskarevskiy Andrey <laskarevskiy73@gmail.com>
2019-07-17 22:35:20 +0300

    Updates

---
+++ precache-manifest.e1c8aca5bc4d5cf473c2a1f869af0bbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e5c9bc86b77bc4071b6e4faf86be0c",
+    "url": "/goit-react-hw-04-advanced-reader/index.html"
+  },
+  {
+    "revision": "83d6bbc2a8596a8e3f36",
+    "url": "/goit-react-hw-04-advanced-reader/static/css/main.005eef0a.chunk.css"
+  },
+  {
+    "revision": "5dafef4200fd3843e7dd",
+    "url": "/goit-react-hw-04-advanced-reader/static/js/2.9c4f0b01.chunk.js"
+  },
+  {
+    "revision": "83d6bbc2a8596a8e3f36",
+    "url": "/goit-react-hw-04-advanced-reader/static/js/main.a4503e9e.chunk.js"
+  },
+  {
+    "revision": "aff4cabbe781ab518317",
+    "url": "/goit-react-hw-04-advanced-reader/static/js/runtime~main.cc318e79.js"
+  }
+]);
