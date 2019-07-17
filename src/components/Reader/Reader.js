6601d6ecb20015371a95ba9b0a7062e03eab8343
Laskarevskiy Andrey <laskarevskiy73@gmail.com>
2019-07-17 22:38:02 +0300

    fix end of line in LF

--- src/components/Reader/Reader.js
+++ src/components/Reader/Reader.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 import queryString from 'query-string';
 import PropTypes from 'prop-types';
 import Publication from './Publication';
 import Counter from './Counter';
 import Controls from './Controls';
 import allPublication from './publication.json';
+import popTransition from '../../transition/pop.module.css';
 
-const parseQueryString = currentPublication => {
-  return Number(queryString.parse(currentPublication).item);
+const parseQueryString = (currentPublication, startPublication) => {
+  return currentPublication
+    ? Number(queryString.parse(currentPublication).item)
+    : Number(startPublication);
 };
 
 class Reader extends Component {
@@ -25,15 +29,27 @@ class Reader extends Component {
   state = {
     currentPublication: 1,
     publication: allPublication,
+    animation: true,
   };
 
   componentDidMount() {
     const { currentPublication } = this.state;
     const { history, location } = this.props;
+    const stringQuery = parseQueryString(location.search, currentPublication);
+
+    if (stringQuery !== currentPublication) {
+      this.setState({ currentPublication: stringQuery });
+
+      history.push({
+        ...location,
+        search: `?item=${stringQuery}`,
+      });
+
+      return;
+    }
 
     history.push({
       ...location,
-      pathname: '/reader',
       search: `?item=${currentPublication}`,
     });
   }
@@ -51,28 +67,39 @@ class Reader extends Component {
       ...location,
       search: `?item=${currentPublication}`,
     });
+
+    this.animationState();
   }
 
   handleIncrement = () => {
     const parseString = parseQueryString(this.props.location.search);
 
-    this.setState({ currentPublication: parseString - 1 });
+    this.setState({ currentPublication: parseString - 1, animation: false });
   };
 
   handleDicrement = () => {
     const parseString = parseQueryString(this.props.location.search);
 
-    this.setState({ currentPublication: parseString + 1 });
+    this.setState({ currentPublication: parseString + 1, animation: false });
+  };
+
+  animationState = () => {
+    this.setState({ animation: true });
   };
 
   render() {
-    const { currentPublication, publication } = this.state;
+    const { currentPublication, publication, animation } = this.state;
 
     return (
       <>
-        <div>Reader Page</div>
-
-        <Publication currentItem={publication[currentPublication - 1]} />
+        <CSSTransition
+          in={animation}
+          timeout={600}
+          classNames={popTransition}
+          unmountOnExit
+        >
+          <Publication currentItem={publication[currentPublication - 1]} />
+        </CSSTransition>
         <Counter
           currentItem={currentPublication}
           itemsLength={publication.length}
